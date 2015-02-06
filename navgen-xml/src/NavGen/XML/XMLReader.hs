98cd903a447547356bd1e92bd48d100f4189c527
jonpetterbergman <jon.petter.bergman@gmail.com>
2015-02-06 07:51:54 +0100

    fixed cabal build-dependencies, xmlFileReader

---
+++ navgen-xml/src/NavGen/XML/XMLReader.hs
@@ -0,0 +1,15 @@
+module NavGen.XML.XMLReader where
+
+import           NavGen.IO               (FileReader)
+import qualified Data.ByteString.Lazy as  LB
+import           Text.XML.Light.Input    (parseXMLDoc)
+import           Text.XML.Light.Blaze    (elementToBlaze)
+import           Text.Blaze.Html         (Html)
+
+xmlFileReader :: FileReader ([String],Html)
+xmlFileReader filename =
+  do
+    mDoc <- fmap parseXMLDoc $ LB.readFile filename 
+    case mDoc of
+      Nothing -> error "parseXMLDoc returned Nothing"
+      Just doc -> return (Nothing,([],elementToBlaze doc))
