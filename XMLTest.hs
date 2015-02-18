c752adfa0f6fb72443681c07c325b155102e8d85
Petter Bergman <jon.petter.bergman@gmail.com>
2015-02-18 14:28:19 +0100

    more complete test code

--- XMLTest.hs
+++ XMLTest.hs
@@ -2,25 +2,24 @@
 
 module XMLTest where
 
-import           Data.NavZip                    (Level(..),
-                                                 NavZip(..))
-import           Data.NavTree                   (followValue)
-import           NavGen.XML.XMLReader           (xmlFileReader)
-import           NavGen.IO                      (readLocalSites,
-                                                 writePage,
-                                                 copyResource,
-                                                 Template)
-import           NavGen.Data.Navigation         (allPages)
-import qualified Text.Blaze.Html5            as  H
-import qualified Text.Blaze.Html5.Attributes as  HA
-import           Text.Blaze.Html                (toHtml,
-                                                 Html)
+import           Data.NavZip                     (Level(..),
+                                                  NavZip(..))
+import           Data.NavTree                    (followValue)
+import           NavGen.XML.XMLReader            (xmlFileReader)
+import           NavGen.IO                       (readLocalSites,
+                                                  writePage,
+                                                  copyResource,
+                                                  Template,
+                                                  FileWriter)
+import           NavGen.Data.Navigation          (allPages)
+import qualified Text.Blaze.Html5             as  H
+import qualified Text.Blaze.Html5.Attributes  as  HA
+import           Text.Blaze.Html                 (toHtml,
+                                                  Html)
+import           Text.Blaze.Html.Renderer.Pretty (renderHtml)
 
-
-
-blazeWriter :: FileWriter
-blazeWriter filename doc =
-  -- TODO
+blazeWriter :: FileWriter Html
+blazeWriter filename = writeFile filename . renderHtml
 
 testTemplate :: Template ([String],Html) Html
 testTemplate (thisLang,otherLangs,nav) =
@@ -36,5 +35,5 @@ test :: FilePath
 test src dst =
   do
     (resources,pages) <- readLocalSites xmlFileReader src
-    mapM_ (writePage dst testTemplate) $ allPages pages
+    mapM_ (writePage blazeWriter dst testTemplate) $ allPages pages
     mapM_ (copyResource src dst) resources 
