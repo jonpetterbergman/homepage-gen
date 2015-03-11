226c8a32337c1d49a710fc70aef99f476a6f6ba9
Petter Bergman <jon.petter.bergman@gmail.com>
2015-03-11 14:40:25 +0100

    nicePathN

--- XMLTest.hs
+++ XMLTest.hs
@@ -3,7 +3,8 @@
 module XMLTest where
 
 import           Data.NavZip                     (Level(..),
-                                                  NavZip(..))
+                                                  NavZip(..),
+                                                  ancestors)
 import           Data.NavTree                    (followValue,
                                                   key)
 import           NavGen.XML.XMLReader            (xmlFileReader)
@@ -17,30 +18,46 @@ import           NavGen.Data.Site                (Label(..))
 import qualified Text.Blaze.Html5             as  H
 import qualified Text.Blaze.Html5.Attributes  as  HA
 import           Text.Blaze.Html                 (toHtml,
-                                                  Html)
+                                                  Html,
+                                                  (!))
 import           Text.Blaze.Html.Renderer.Pretty (renderHtml)
+import           Data.List                       (intercalate,
+                                                  intersperse)
+import           Data.Monoid                     (mconcat)
 
+getTitle :: NavZip (Label a) b
+         -> a
 getTitle = nicename . key . here . level
 
-nicePath = map getTitle . reverse . ancestors
+nicePath :: NavZip (Label String) a
+         -> String
+nicePath x = intercalate "/" $ (map getTitle $ reverse $ ancestors x) ++ [getTitle x]
+
+nicePathN :: NavZip (Label String) a
+          -> Html
+nicePathN x = mconcat $ intersperse "/" $ (map go $ reverse $ ancestors x) ++ [toHtml $ getTitle x]
+  where go x = H.a ! HA.href (H.toValue $ urlname $ key $ here $ level x) $ toHtml $ getTitle x
 
 blazeWriter :: FileWriter Html
 blazeWriter filename = writeFile filename . renderHtml
 
-testTemplate :: Template ([String],Html) Html
-testTemplate (thisLang,otherLangs,nav) =
+nicePathTemplate :: Template ([String],Html) Html
+nicePathTemplate (thisLang,otherLangs,nav) =
   let title = nicePath nav in
   H.docTypeHtml $ do
-    H.head $ H.title title
+    H.head $ H.title $ H.toHtml title
     H.body $ do
-      H.h1 title
+      H.h1 $ nicePathN nav
       H.div $ snd $ followValue $ here $ level nav
 
-test :: FilePath
-     -> FilePath 
-     -> IO ()
-test src dst =
+testNicePath :: FilePath
+             -> FilePath 
+             -> IO ()
+testNicePath src dst =
   do
     (resources,pages) <- readLocalSites xmlFileReader src
-    mapM_ (writePage blazeWriter dst testTemplate) $ allPages pages
+    mapM_ (writePage blazeWriter dst nicePathTemplate) $ allPages pages
     mapM_ (copyResource src dst) resources 
+
+--menu :: NavZip (Label String) a
+--     -> Forest (Label String)
